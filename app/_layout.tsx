c9f479357965c8d4a1e24f7a5589b3a9ace6f07e
tswxnxj <sttctmrsn7@privaterelay.appleid.com>
2026-04-14 16:46:23 +0900

    fix: 로그인 후에만 setupNotifications 호출

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,6 @@ export default function RootLayout() {
 
   useEffect(() => {
     initializeKakaoSDK('e762d1d9bb4d6747b68bef981907a99a');
-    setupNotifications();
   }, []);
 
   useEffect(() => {
@@ -25,6 +24,7 @@ export default function RootLayout() {
   useEffect(() => {
     if (initializing) return;
     if (user) {
+      setupNotifications();
       router.replace('/(tabs)');
     } else {
       router.replace('/(auth)/login');
